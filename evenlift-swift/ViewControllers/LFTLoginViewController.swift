655324b0e493a97572bb180a92b329f30c5af7a1
Gianni Settino <g@miltonandparc.com>
2014-08-18 22:24:08 +0200

    Implement basic login system

--- evenlift-swift/ViewControllers/LFTLoginViewController.swift
+++ evenlift-swift/ViewControllers/LFTLoginViewController.swift
@@ -10,18 +10,54 @@ import UIKit
 
 class LFTLoginViewController: UIViewController {
     
-    init(coder aDecoder: NSCoder!) {
-        // do shit here
-        
-        super.init(coder: aDecoder)
-    }
+    var firebase = Firebase(url: "https://evenlift.firebaseio.com/")
+    var authClient: FirebaseSimpleLogin?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        authClient = FirebaseSimpleLogin(ref: firebase)
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
+        
+        // Log user in if he isn't already
+        authClient!.checkAuthStatusWithBlock({
+            (error: NSError!, user: FAUser!) in
+            if (error) {
+                // there was an error
+                self.showAlertWithTitle("Error", message: error.description)
+            } else if (!user) {
+                // user is not logged in
+                self.login()
+            } else {
+                // user is logged in
+                self.launchApp()
+            }
+            })
+    }
+    
+    func login() {
+        authClient!.loginToFacebookAppWithId("420007321469839", permissions: nil, audience: ACFacebookAudienceFriends, withCompletionBlock:{
+            (error: NSError!, user: FAUser!) in
+            if (error) {
+                // there was an error
+                self.showAlertWithTitle("Error", message: error.description)
+            } else {
+                // login successful
+                self.launchApp()
+            }
+            })
+    }
+    
+    func launchApp() {
+        showAlertWithTitle("Success", message: "Fuck yeah.")
+    }
+    
+    func showAlertWithTitle(title: String, message: String) {
+        var alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        self.presentViewController(alert, animated: true, completion: nil)
     }
     
 }

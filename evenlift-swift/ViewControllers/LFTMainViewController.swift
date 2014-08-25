04fc21f6cadccdddb4bd5cc76db110c8e2c25ed9
Gianni Settino <g@miltonandparc.com>
2014-08-25 13:34:35 -0400

    Redesign!

--- evenlift-swift/ViewControllers/LFTMainViewController.swift
+++ evenlift-swift/ViewControllers/LFTMainViewController.swift
@@ -33,51 +33,42 @@ class LFTMainViewController: UIViewController {
         let iphoneHeight = 568-64 // to account for status and nav bars
         let iphoneWidth = 320
         
-        let padding = 10 // this value dictates everything!
-        let otherButtonHeight = 44
-        let buttonWidth = (iphoneWidth-(3*padding))/2
-        let buttonHeight = (iphoneHeight-(4*padding)-otherButtonHeight)/2
+        let buttonHeight = iphoneHeight/4
         
         // First button (Bench)
-        var benchButton = FUIButton(frame: CGRect(x: padding, y: padding, width: buttonWidth, height: buttonHeight))
+        var benchButton = FUIButton(frame: CGRect(x: 0, y: 0, width: iphoneWidth, height: buttonHeight))
         styleButton(&benchButton, withTitle: "BENCH")
+        benchButton.buttonColor = UIColor.peterRiverColor()
+        benchButton.highlightedColor = UIColor.belizeHoleColor()
         view.addSubview(benchButton)
         
         // Second button (Squat)
-        var squatButton = FUIButton(frame: CGRect(x: (2*padding)+buttonWidth, y: padding, width: buttonWidth, height: buttonHeight))
+        var squatButton = FUIButton(frame: CGRect(x: 0, y: buttonHeight, width: iphoneWidth, height: buttonHeight))
         styleButton(&squatButton, withTitle: "SQUAT")
+        squatButton.buttonColor = UIColor.turquoiseColor()
+        squatButton.highlightedColor = UIColor.greenSeaColor()
         view.addSubview(squatButton)
         
         // Third button (Press)
-        var pressButton = FUIButton(frame: CGRect(x: padding, y: (2*padding)+buttonHeight, width: buttonWidth, height: buttonHeight))
+        var pressButton = FUIButton(frame: CGRect(x: 0, y: 2*buttonHeight, width: iphoneWidth, height: buttonHeight))
         styleButton(&pressButton, withTitle: "PRESS")
+        pressButton.buttonColor = UIColor.alizarinColor()
+        pressButton.highlightedColor = UIColor.pomegranateColor()
         view.addSubview(pressButton)
         
         // Fourth button (Deadlift)
-        var dlButton = FUIButton(frame: CGRect(x: (2*padding)+buttonWidth, y: (2*padding)+buttonHeight, width: buttonWidth, height: buttonHeight))
+        var dlButton = FUIButton(frame: CGRect(x: 0, y: 3*buttonHeight, width: iphoneWidth, height: buttonHeight))
         styleButton(&dlButton, withTitle: "DEADLIFT")
+        dlButton.buttonColor = UIColor.amethystColor()
+        dlButton.highlightedColor = UIColor.wisteriaColor()
         view.addSubview(dlButton)
-        
-        // Bottom button (Other)
-        /*var otherButton = FUIButton(frame: CGRect(x: padding, y: (3*padding)+(2*buttonHeight), width: iphoneWidth-(2*padding), height: otherButtonHeight))
-        styleButton(&otherButton)
-        otherButton.buttonColor = UIColor.peterRiverColor()
-        otherButton.shadowColor = UIColor.belizeHoleColor()
-        view.addSubview(otherButton)*/
     }
     
     func styleButton(inout button: FUIButton, withTitle title: String) {
-        button.buttonColor = UIColor.turquoiseColor()
-        button.shadowColor = UIColor.greenSeaColor()
-        button.shadowHeight = 3
-        button.cornerRadius = 6
-        button.titleLabel.font = UIFont.boldFlatFontOfSize(20)
-        button.setTitleColor(UIColor.cloudsColor(), forState: .Normal)
-        
         var titleLabel = UILabel(frame: CGRect(x: 10, y: 10, width: 0, height: 0))
         titleLabel.text = title
         titleLabel.textColor = UIColor.cloudsColor()
-        titleLabel.font = UIFont.boldFlatFontOfSize(14)
+        titleLabel.font = UIFont.boldFlatFontOfSize(12)
         titleLabel.sizeToFit()
         titleLabel.frame.origin.x = (button.frame.width/2) - (titleLabel.frame.width/2)
         button.addSubview(titleLabel)

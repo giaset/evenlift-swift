3cae139603cb937105840b1eb03c75e34d2b1ec4
Gianni Settino <g@miltonandparc.com>
2014-09-01 00:48:48 -0400

    Re-redesign

--- evenlift-swift/ViewControllers/LFTMainViewController.swift
+++ evenlift-swift/ViewControllers/LFTMainViewController.swift
@@ -10,19 +10,21 @@ import UIKit
 
 class LFTMainViewController: UIViewController {
     
+    var darkColor = UIColor(fromHexCode: "303133")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Evenlift"
         
-        view.backgroundColor = UIColor.cloudsColor()
+        view.backgroundColor = darkColor
         
         styleNavBar()
         setupButtons()
     }
     
     func styleNavBar() {
-        navigationController.navigationBar.configureFlatNavigationBarWithColor(UIColor.midnightBlueColor())
+        navigationController.navigationBar.configureFlatNavigationBarWithColor(darkColor)
         var attributes = NSMutableDictionary(dictionary: navigationController.navigationBar.titleTextAttributes)
         attributes.setValue(UIFont.boldFlatFontOfSize(18), forKey: NSFontAttributeName)
         attributes.setValue(UIColor.whiteColor(), forKey: NSForegroundColorAttributeName)
@@ -34,9 +36,8 @@ class LFTMainViewController: UIViewController {
         let iphoneWidth = 320
         
         let padding = 10 // this value dictates everything!
-        let otherButtonHeight = 44
         let buttonWidth = (iphoneWidth-(3*padding))/2
-        let buttonHeight = (iphoneHeight-(4*padding)-otherButtonHeight)/2
+        let buttonHeight = buttonWidth
         
         // First button (Bench)
         var benchButton = FUIButton(frame: CGRect(x: padding, y: padding, width: buttonWidth, height: buttonHeight))
@@ -57,30 +58,51 @@ class LFTMainViewController: UIViewController {
         var dlButton = FUIButton(frame: CGRect(x: (2*padding)+buttonWidth, y: (2*padding)+buttonHeight, width: buttonWidth, height: buttonHeight))
         styleButton(&dlButton, withTitle: "DEADLIFT")
         view.addSubview(dlButton)
-        
-        // Bottom button (Other)
-        /*var otherButton = FUIButton(frame: CGRect(x: padding, y: (3*padding)+(2*buttonHeight), width: iphoneWidth-(2*padding), height: otherButtonHeight))
-        styleButton(&otherButton)
-        otherButton.buttonColor = UIColor.peterRiverColor()
-        otherButton.shadowColor = UIColor.belizeHoleColor()
-        view.addSubview(otherButton)*/
     }
     
     func styleButton(inout button: FUIButton, withTitle title: String) {
-        button.buttonColor = UIColor.turquoiseColor()
-        button.shadowColor = UIColor.greenSeaColor()
-        button.shadowHeight = 3
-        button.cornerRadius = 6
-        button.titleLabel.font = UIFont.boldFlatFontOfSize(20)
-        button.setTitleColor(UIColor.cloudsColor(), forState: .Normal)
+        button.buttonColor = UIColor.cloudsColor()
+        button.cornerRadius = 1
         
+        var highlightColor: UIColor
+        switch title {
+        case "BENCH":
+            highlightColor = UIColor.alizarinColor()
+        case "SQUAT":
+            highlightColor = UIColor.nephritisColor()
+        case "PRESS":
+            highlightColor = UIColor.peterRiverColor()
+        case "DEADLIFT":
+            highlightColor = UIColor.sunflowerColor()
+        default:
+            highlightColor = darkColor
+        }
+        
+        /* TITLE LABEL */
         var titleLabel = UILabel(frame: CGRect(x: 10, y: 10, width: 0, height: 0))
         titleLabel.text = title
-        titleLabel.textColor = UIColor.cloudsColor()
+        titleLabel.textColor = highlightColor
         titleLabel.font = UIFont.boldFlatFontOfSize(14)
         titleLabel.sizeToFit()
         titleLabel.frame.origin.x = (button.frame.width/2) - (titleLabel.frame.width/2)
         button.addSubview(titleLabel)
+        
+        /* 1RM LABEL */
+        var repMaxLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        repMaxLabel.text = "275"
+        repMaxLabel.textColor = darkColor
+        repMaxLabel.font = UIFont.boldFlatFontOfSize(72)
+        repMaxLabel.sizeToFit()
+        repMaxLabel.frame.origin.x = (button.frame.width/2) - (repMaxLabel.frame.width/2)
+        repMaxLabel.frame.origin.y = (button.frame.height/2) - (repMaxLabel.frame.height/2)
+        button.addSubview(repMaxLabel)
+        
+        /* PROGRESS VIEW */
+        let progressViewHeight: CGFloat = 5
+        var progressView = UIView(frame: CGRect(x: 3, y: button.frame.height-progressViewHeight-3, width: button.frame.width-15, height: progressViewHeight))
+        progressView.backgroundColor = highlightColor
+        progressView.layer.cornerRadius = 1
+        button.addSubview(progressView)
     }
     
 }

9fb9d8b61c035c26d44d2d114ec95c7712fd2c1c
Gianni Settino <g@miltonandparc.com>
2014-09-03 00:52:54 -0400

    Start externalizing dashboard tiles

---
+++ evenlift-swift/Custom Views/LFTDashboardTile.swift
@@ -0,0 +1,13 @@
+//
+//  LFTDashboardTile.swift
+//  evenlift-swift
+//
+//  Created by Gianni Settino on 2014-09-01.
+//  Copyright (c) 2014 Milton and Parc. All rights reserved.
+//
+
+import UIKit
+
+class LFTDashboardTile: FUIButton {
+    
+}

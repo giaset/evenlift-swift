db34b639c5896b838cf4beafef4f8b84447b189c
Gianni Settino <g@miltonandparc.com>
2014-09-10 19:17:56 -0400

    Externalize view logic into LFTDashboardTile

--- evenlift-swift/Custom Views/LFTDashboardTile.swift
+++ evenlift-swift/Custom Views/LFTDashboardTile.swift
@@ -8,6 +8,85 @@
 
 import UIKit
 
-class LFTDashboardTile: FUIButton {
+class LFTDashboardTile: UIView {
+    
+    func styleForExercise(exercise: String) {
+        var backgroundColor: UIColor
+        var highlightColor = UIColor.whiteColor()
+        
+        switch exercise {
+        case "BENCH":
+            backgroundColor = UIColor(fromHexCode: "4aa8d8")
+        case "SQUAT":
+            backgroundColor = UIColor(fromHexCode: "56bc8a")
+        case "DEADLIFT":
+            backgroundColor = UIColor(fromHexCode: "d95e40")
+        default:
+            backgroundColor = UIColor.whiteColor()
+            highlightColor = UIColor(fromHexCode: "3b4c61")
+        }
+        
+        self.backgroundColor = backgroundColor
+        
+        /* TITLE LABEL */
+        var titleLabel = UILabel(frame: CGRect(x: 0, y: 10, width: 0, height: 0))
+        titleLabel.text = exercise
+        titleLabel.textColor = highlightColor
+        titleLabel.font = UIFont.boldFlatFontOfSize(18)
+        titleLabel.sizeToFit()
+        titleLabel.frame.origin.x = (frame.width/2) - (titleLabel.frame.width/2)
+        addSubview(titleLabel)
+        
+        /* 1RM LABEL */
+        var repMaxLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        repMaxLabel.text = "333"
+        repMaxLabel.textColor = highlightColor
+        repMaxLabel.font = UIFont.boldFlatFontOfSize(100)
+        repMaxLabel.sizeToFit()
+        repMaxLabel.frame.origin.x = (frame.width/2) - (repMaxLabel.frame.width/2)
+        repMaxLabel.frame.origin.y = (frame.height/2) - (repMaxLabel.frame.height/2)
+        addSubview(repMaxLabel)
+        
+        /* UNIT LABEL */
+        var unitLabel = UILabel(frame: CGRect(x: repMaxLabel.frame.maxX+2, y: repMaxLabel.frame.minY+20, width: 0, height: 0))
+        unitLabel.text = "LBS"
+        unitLabel.textColor = highlightColor
+        unitLabel.font = UIFont.flatFontOfSize(18)
+        unitLabel.sizeToFit()
+        addSubview(unitLabel)
+        
+        /* PROGRESS VIEW SKELETON */
+        let progressViewMargin: CGFloat = 10
+        let progressViewHeight: CGFloat = 10
+        let progressViewMaxWidth = frame.width-(2*progressViewMargin)
+        let progressViewY = frame.height-progressViewHeight-progressViewMargin
+        var progressView = UIView(frame: CGRect(x: progressViewMargin, y: progressViewY, width: progressViewMaxWidth, height: progressViewHeight))
+        progressView.layer.borderColor = highlightColor.CGColor
+        progressView.layer.borderWidth = 1
+        addSubview(progressView)
+        
+        /* ACTUAL PROGRESS BAR */
+        let percentage: CGFloat = 0.625
+        var progressBar = UIView(frame: CGRect(x: progressViewMargin, y: progressViewY, width: progressViewMaxWidth*percentage, height: progressViewHeight))
+        progressBar.backgroundColor = highlightColor
+        addSubview(progressBar)
+        
+        /* LITTLE NOTCHES ON PROGRESS VIEW */
+        let notchWidth: CGFloat = 1
+        let notchSpacing = progressViewMaxWidth/4
+        
+        var notch1 = UIView(frame: CGRect(x: progressViewMargin+notchSpacing, y: progressViewY, width: notchWidth, height: progressViewHeight))
+        notch1.backgroundColor = backgroundColor
+        addSubview(notch1)
+        
+        var notch2 = UIView(frame: CGRect(x: progressViewMargin+(2*notchSpacing), y: progressViewY, width: notchWidth, height: progressViewHeight))
+        notch2.backgroundColor = backgroundColor
+        addSubview(notch2)
+        
+        var notch3 = UIView(frame: CGRect(x: progressViewMargin+(3*notchSpacing), y: progressViewY, width: notchWidth, height: progressViewHeight))
+        notch3.backgroundColor = highlightColor
+        addSubview(notch3)
+
+    }
     
 }
--- evenlift-swift/ViewControllers/LFTMainViewController.swift
+++ evenlift-swift/ViewControllers/LFTMainViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class LFTMainViewController: UIViewController {
     
-    var darkColor = UIColor(fromHexCode: "303133")
+    var darkColor = UIColor(fromHexCode: "3b4c61")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,13 +20,6 @@ class LFTMainViewController: UIViewController {
         navigationController.navigationBarHidden = true
         
         setupButtons()
-        setupStatusBar()
-    }
-    
-    func setupStatusBar() {
-        var statusBarView = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 20))
-        statusBarView.backgroundColor = darkColor
-        view.addSubview(statusBarView)
     }
     
     func setupButtons() {
@@ -42,96 +35,30 @@ class LFTMainViewController: UIViewController {
         let buttonWidth: CGFloat = iphoneWidth-(2*horizontalPadding)
         let buttonHeight: CGFloat = buttonWidth
         
-        let verticalPadding: CGFloat = (iphoneHeight/2)-(buttonHeight/2)-20
+        let verticalPadding: CGFloat = (iphoneHeight/2)-(buttonHeight/2)-20 // - 20 for the status bar
         
         // First tile (Bench)
-        var benchTile = UIView(frame: CGRect(x: horizontalPadding, y: verticalPadding, width: buttonWidth, height: buttonHeight))
-        styleTile(&benchTile, withTitle: "BENCH")
+        var benchTile = LFTDashboardTile(frame: CGRect(x: horizontalPadding, y: verticalPadding, width: buttonWidth, height: buttonHeight))
+        benchTile.styleForExercise("BENCH")
         scrollview.addSubview(benchTile)
         
         // Second tile (Squat)
-        var squatTile = UIView(frame: CGRect(x: iphoneWidth+horizontalPadding, y: verticalPadding, width: buttonWidth, height: buttonHeight))
-        styleTile(&squatTile, withTitle: "SQUAT")
+        var squatTile = LFTDashboardTile(frame: CGRect(x: iphoneWidth+horizontalPadding, y: verticalPadding, width: buttonWidth, height: buttonHeight))
+        squatTile.styleForExercise("SQUAT")
         scrollview.addSubview(squatTile)
         
         // Third tile (Press)
-        var pressTile = UIView(frame: CGRect(x: (2*iphoneWidth)+horizontalPadding, y: verticalPadding, width: buttonWidth, height: buttonHeight))
-        styleTile(&pressTile, withTitle: "PRESS")
+        var pressTile = LFTDashboardTile(frame: CGRect(x: (2*iphoneWidth)+horizontalPadding, y: verticalPadding, width: buttonWidth, height: buttonHeight))
+        pressTile.styleForExercise("PRESS")
         scrollview.addSubview(pressTile)
         
         // Fourth tile (Deadlift)
-        var dlTile = UIView(frame: CGRect(x: (3*iphoneWidth)+horizontalPadding, y: verticalPadding, width: buttonWidth, height: buttonHeight))
-        styleTile(&dlTile, withTitle: "DEADLIFT")
+        var dlTile = LFTDashboardTile(frame: CGRect(x: (3*iphoneWidth)+horizontalPadding, y: verticalPadding, width: buttonWidth, height: buttonHeight))
+        dlTile.styleForExercise("DEADLIFT")
         scrollview.addSubview(dlTile)
         
         scrollview.contentSize = CGSize(width: 4*iphoneWidth, height: 1)
         view.addSubview(scrollview)
-    }
-    
-    func styleTile(inout tile: UIView, withTitle title: String) {
-        var backgroundColor: UIColor
-        switch title {
-        case "BENCH":
-            backgroundColor = UIColor(fromHexCode: "F3545B")
-        case "SQUAT":
-            backgroundColor = UIColor(fromHexCode: "628B85")
-        case "PRESS":
-            backgroundColor = UIColor(fromHexCode: "366387")
-        case "DEADLIFT":
-            backgroundColor = UIColor(fromHexCode: "FBDE5E")
-        default:
-            backgroundColor = darkColor
-        }
-        
-        tile.backgroundColor = backgroundColor
-        
-        /* TITLE LABEL */
-        var titleLabel = UILabel(frame: CGRect(x: 0, y: 10, width: 0, height: 0))
-        titleLabel.text = title
-        titleLabel.textColor = UIColor.cloudsColor()
-        titleLabel.font = UIFont.boldFlatFontOfSize(18)
-        titleLabel.sizeToFit()
-        titleLabel.frame.origin.x = (tile.frame.width/2) - (titleLabel.frame.width/2)
-        tile.addSubview(titleLabel)
-        
-        /* 1RM LABEL */
-        var repMaxLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
-        repMaxLabel.text = "275"
-        repMaxLabel.textColor = UIColor.cloudsColor()
-        repMaxLabel.font = UIFont.boldFlatFontOfSize(100)
-        repMaxLabel.sizeToFit()
-        repMaxLabel.frame.origin.x = (tile.frame.width/2) - (repMaxLabel.frame.width/2)
-        repMaxLabel.frame.origin.y = (tile.frame.height/2) - (repMaxLabel.frame.height/2)
-        tile.addSubview(repMaxLabel)
-        
-        /* UNIT LABEL */
-        var unitLabel = UILabel(frame: CGRect(x: repMaxLabel.frame.maxX+2, y: repMaxLabel.frame.minY+20, width: 0, height: 0))
-        unitLabel.text = "LBS"
-        unitLabel.textColor = UIColor.cloudsColor()
-        unitLabel.font = UIFont.flatFontOfSize(18)
-        unitLabel.sizeToFit()
-        tile.addSubview(unitLabel)
-        
-        /* PROGRESS VIEW */
-        let progressViewMargin: CGFloat = 5
-        let progressViewHeight: CGFloat = 10
-        let progressViewMaxWidth = tile.frame.width-(2*progressViewMargin)
-        let progressViewY = tile.frame.height-progressViewHeight-progressViewMargin
-        var progressView = UIView(frame: CGRect(x: progressViewMargin, y: progressViewY, width: progressViewMaxWidth*0.625, height: progressViewHeight))
-        progressView.backgroundColor = UIColor.cloudsColor()
-        tile.addSubview(progressView)
-        
-        /* LITTLE NOTCHES ON PROGRESS VIEW */
-        let notchWidth: CGFloat = 1
-        let notchSpacing = progressViewMaxWidth/4
-        
-        var notch1 = UIView(frame: CGRect(x: progressViewMargin+notchSpacing, y: progressViewY, width: notchWidth, height: progressViewHeight))
-        notch1.backgroundColor = backgroundColor
-        tile.addSubview(notch1)
-        
-        var notch2 = UIView(frame: CGRect(x: progressViewMargin+(2*notchSpacing), y: progressViewY, width: notchWidth, height: progressViewHeight))
-        notch2.backgroundColor = backgroundColor
-        tile.addSubview(notch2)
     }
     
 }
